7444934bedb9b3c1f6fa06eb2b665ad1663d24c0
blackbeans <blackbeans.zc@gmail.com>
2016-02-22 18:08:08 +0800

    modified:   build.sh

--- build.sh
+++ build.sh
@@ -2,3 +2,6 @@ go get github.com/naoina/toml
 go get github.com/blackbeans/turbo
 go get github.com/blackbeans/log4go
 go get gopkg.in/redis.v3 		#只用来测试
+
+go build go-moa/core
+go build go-moa/protocol
